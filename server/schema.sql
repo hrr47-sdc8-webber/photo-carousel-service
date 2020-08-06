0c14c7dfaf34db0de05960ef10b4c415749aeac5
Jackson Todd <jacksonttodd@gmail.com>
2020-08-06 19:33:46 -0400

    Revised for greater consistency and clarity in naming convention.

--- server/schema.sql
+++ server/schema.sql
@@ -5,15 +5,15 @@ CREATE DATABASE zagat;
 USE zagat;
 
 CREATE TABLE Restaurants (
-  id int NOT NULL AUTO_INCREMENT,
+  Restaurant_id int NOT NULL AUTO_INCREMENT,
   Restaurant_Name varchar(30) NOT NULL,
   PRIMARY KEY(Restaurant_id)
 );
 
-CREATE TABLE Photos (
-  id int NOT NULL AUTO_INCREMENT,
-  img_url char(12), --image URL will only contain the unique identifier. example: 'img0000.jpeg'
+CREATE TABLE Images (
+  Image_id int NOT NULL AUTO_INCREMENT,
+  Image_url char(12), --image URL will only contain the unique identifier. example: 'img0000.jpeg'
   Restaurant_id int,
-  PRIMARY KEY(id),
-  FOREIGN KEY(Restaurant_id) REFERENCES Restaurants(id)
+  PRIMARY KEY(Image_id),
+  FOREIGN KEY(Restaurant_id) REFERENCES Restaurants(Restaurant_id)
 );
