3c1d8821283fdc1dbcfc2aea4179c5b606d123cd
Jackson Todd <jacksonttodd@gmail.com>
2020-08-07 14:12:28 -0400

    Removed redundant comments from seed script

--- server/seed.js
+++ server/seed.js
@@ -6,12 +6,10 @@ const db = require(`./database/database.js`);
 for (let i = 1; i <= 100; i++) {
   // the value of i will be used as the restaurant id
   let restaurantName = faker.company.companyName();
-  restaurantName = restaurantName.replace(/'/g, "''");
   // replace single quote in restaurantName with two single quotes, otherwise mySql will error
+  restaurantName = restaurantName.replace(/'/g, "''");
   db.insertRestaurant(restaurantName, i);
-  // restaurantName must be inserted first due to foreign key restraints
   const imageCount = Math.floor(10 + 8 * Math.random());
-  // Each restaurant will have between 10 and 18 images. The exact number within this range is randomly selected.
   for (let j = 0; j < imageCount; j++) {
     let imageNumber = Math.floor(49 * Math.random());
     let imageURL = `img00${imageNumber}.jpeg`;
